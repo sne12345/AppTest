3a5ae16a9d5c6791d553c0d6158df09ec859e62b
MinyeongHonggit config --global user.email agka00161@gmail.comcd C:/Users/user/Desktop/Git <78523467+MinyeongHong@users.noreply.github.com>
2021-06-23 16:12:01 +0900

    first commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        Button btn_logout  = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //로그아웃
+                mFirebaseAuth.signOut();
+
+                Intent intent = new Intent(MainActivity.this, Start.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        //탈퇴
+        //mFirebaseAuth.getCurrentUser().delete();
+
+    }
+}
