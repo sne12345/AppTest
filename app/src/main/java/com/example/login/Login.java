8b5df096fe4a0a3b06070a41dfe7a1fec4fb6c50
MinyeongHonggit config --global user.email agka00161@gmail.comcd C:/Users/user/Desktop/Git <78523467+MinyeongHong@users.noreply.github.com>
2021-06-29 13:40:49 +0900

    add background image

--- app/src/main/java/com/example/login/Login.java
+++ app/src/main/java/com/example/login/Login.java
@@ -10,67 +10,68 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
 
 public class Login extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
-    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
-    private EditText mEtEmail, mEtPwd;//로그인 입력필드
     private Toast toast;
-
+    private static final String TAG = "AnonymousAuth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        //mDatabaseRef = FirebaseDatabase.getInstance().getReference();
 
-        mEtEmail = findViewById(R.id.et_email);
-        mEtPwd = findViewById(R.id.et_pwd);
-
-        Button btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        Button btn_start=findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //로그인 요청
-
-                String strEmail = mEtEmail.getText().toString();
-                String strPwd = mEtPwd.getText().toString();
-
-                mFirebaseAuth.signInWithEmailAndPassword(strEmail,strPwd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                        //로그인 성공시
-                        toast =Toast.makeText(Login.this, "Login Succeed", Toast.LENGTH_SHORT);
-                        toast.setGravity(Gravity.CENTER, 0, -200);
-                        toast.show();
-                        Intent intent = new Intent(Login.this, MainActivity.class);
-                        startActivity(intent);
-                        finish(); //현재 로그인 액티비티 파괴
-
-                    }
-                    else{
-                        toast = Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_LONG);
-                        toast.setGravity(Gravity.CENTER, 0, -200);
-                        toast.show();
-                    }
-                    }
-                });
-
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
 
+        }
+
+    @Override
+    protected void onStart() {
+        super.onStart();         // Check if user is signed in (non-null) and update UI accordingly
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+        updateUI(currentUser);
     }
+
+    private void updateUI(FirebaseUser user) {
+        if (user==null){
+            mFirebaseAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(Login.this, "Hello! Wellcome Tokic",Toast.LENGTH_SHORT);
+                        FirebaseUser user=mFirebaseAuth.getCurrentUser();
+                        updateUI(user);
+                    }
+                    else {
+                        updateUI(null);
+                    }
+                }
+            });
+        }
+
+    }
+
 }
+
